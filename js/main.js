226823abf92a6739fc774e88e354f0aeaaa2db18
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-26 14:09:28 +0200

    sketch mail task

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('js test')
+/* TASK MAIL CHECK */
+//1. create an array with 3 mail address and store in a variable
+
+//2. ask user mail address
+
+//3. compare with the list
+
+//4. if user input is an array's element do access, else don't.
+
