8d6d5a5df8fa2ac901a23dc355b991b0edd03cbc
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-26 14:59:24 +0200

    add completed email task

--- js/main.js
+++ js/main.js
@@ -3,13 +3,35 @@
 const emailList = ['cicciopasticcio@gmail.com', 'tiziocaio@hotmail.com', 'pincopallino@gmail.com'];
 console.log(emailList);
 
-//2. select in html user input of mail address
-const userEmailInput = document.getElementById('mail');
-const userEmail = userEmailInput.value.trim().toLowerCase();
-console.log(userEmail);
+//2. add event click on button Log In
+const btnLogIn = document.querySelector('.access');
+console.log(btnLogIn);
+btnLogIn.addEventListener('click', function() {
+    //3. select in html user input of mail address
+    const userEmailInput = document.getElementById('mail');
+    const userEmail = userEmailInput.value.trim().toLowerCase();
+    console.log(userEmail);
 
-
-//3. compare with the list
-
-//4. if user input is an array's element do access, else don't.
-
+    //4. compare with the list
+    if (userEmail === '') {
+        alert('Error. Plase type a valid address')
+    }
+    //5. if user input is an array's element do access, else don't.
+    else {
+        let userFound = false;
+        const feedback = document.getElementById('feedback');
+        for(let i = 0; i < emailList.length; i++) {
+            if (userEmail === emailList[i]) {
+                userFound = true;
+                break;
+            }
+        }
+        //6. Print feedback in html document 
+        if (userFound === true) {
+            feedback.innerHTML = ': Access succeeded';
+        }
+        else {
+            feedback.innerHTML = ': Access denied';
+        }  
+    }
+})
